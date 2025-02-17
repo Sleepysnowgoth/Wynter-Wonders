e94fca498aded4d896e607e6730eed6470a25053
SleepySnowGoth <108432516+Sleepysnowgoth@users.noreply.github.com>
2025-02-16 19:38:11 -0500

    Add files via upload

---
+++ Ballatro/WynterWonders.lua
@@ -0,0 +1,39 @@
+SMODS.Joker{
+    key = 'crown',
+    loc_txt = {
+        name = 'Crown of Lust',
+        text = {
+            '{X:red,C:white}x#1#{C:red} Mult{}, {C:red}BUT{}',
+            '{C:red}lose {C:money}$#2#{} when scored'
+        }
+    }
+    atlas = 'jokers',
+    rarity = 3
+    cost = 12
+    unlocked = true
+    discovered = true
+    blueprint_compat = true, 
+    eternal_compat = true, 
+    perishable_compat = true,
+    pos = {x = 0, y = 0},
+    config = {extra = {
+        Xmult = 1.5
+    }
+    loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.Xmult,
+	end,
+	calculate = function(self, card, context)
+		if context.individual and context.cardarea == G.play then
+			if ('crown') < G.card.ability.extra. then
+				if context.other_card:is_suit('Hearts') then
+					return{
+						Xmult_mod = card.ability.extra.Xmult,
+						message = 'X' .. card.ability.extra.Xmult,
+						colour = G.C.MULT,
+					}
+				end
+
+			end
+		end
+	end
+}
